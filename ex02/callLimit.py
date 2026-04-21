b6c47b552cea1e850a395b66121d6b42c951b19c
arafaram <arfaram@student.42antananarivo.mg>
2026-04-21 10:19:42 +0300

    rectification et verification d'erreur dans ex02 et ex03

--- ex02/callLimit.py
+++ ex02/callLimit.py
@@ -2,10 +2,14 @@
 
 
 def callLimit(limit: int):
+    """Returns a decorator that limits the number of calls to a function"""
     count = 0
 
     def callLimiter(function):
+        """Wraps the target function to apply the call limit"""
+
         def limit_function(*args: any, **kwds: any):
+            """Executes the function if the limit is not reached"""
             nonlocal count
             if count >= limit:
                 print(f"Error: {function} call too many times")
--- ex03/new_student.py
+++ ex03/new_student.py
@@ -9,11 +9,22 @@ def generate_id() -> str:
 
 @dataclass
 class Student:
+    """Student class"""
+
     name: str
     surname: str
     active: bool = field(default=True)
     login: str = field(default="", init=False)
     id: str = field(default=generate_id(), init=False)
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
+        """Post init method wich is launched after __init__ of dataclass"""
+        if (
+            isinstance(self.name, str) is False
+            or isinstance(self.surname, str) is False
+            or len(self.name) == 0
+            or len(self.surname) == 0
+        ):
+            self.login = "Marvin"
+            return
         self.login = f"{self.name[0]}{self.surname}"
